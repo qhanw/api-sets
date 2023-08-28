3979629528c7a516c850794828f26c9fcc2d5c5e
qhanw <whenhan@foxmail.com>
2023-08-28 20:55:09 +0800

    feat: integrated questionnaire project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { H5Module } from './h5/h5.module';
+import { QtnModule } from './qtn/qtn.module';
+
 @Module({
-  imports: [],
+  imports: [PrismaModule, H5Module, QtnModule],
   controllers: [AppController],
   providers: [AppService],
 })
